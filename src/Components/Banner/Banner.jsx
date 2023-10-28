33180686ba9dfc221cdbeba8a69de137c54ed621
devil-striker <darshaninsta360@gmail.com>
2023-10-28 18:06:16 +0530

    s

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
             <div className='relative -z-10 bg-gradient-to-br to-black from-pink-950  max-h- p-4 flex flex-col items-center justify-center'>
                 <div className={`h-[90vh] w-[90vw] text-white flex justify-center items-center overflow-hidden`}>
                     <div className={`${b.image_container}`}>
-                        <img src="../../../src/Images/myimage.jpg" className='rounded-2xl' alt="no image" width="100%"/>
+                        <img src="/Images/myimage.jpg" className='rounded-2xl' alt="no image" width="100%"/>
                     </div>
                     <div className='relative flex flex-col p-2'>
                         <p className='text-xl text-pink-600 font-bold'>Hello,<br />I'm Darshan Dinesh MP</p>
