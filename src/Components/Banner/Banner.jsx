a48b3918c4a4e16258d8d684235e36feb066fe12
devil-striker <darshaninsta360@gmail.com>
2023-10-28 21:20:14 +0530

    r

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,14 +4,14 @@ import b from './Banner.module.css'
 function Banner() {
     return (
         <>
-            <div className='relative -z-10 bg-gradient-to-br to-black from-pink-950  max-h- p-4 flex flex-col items-center justify-center'>
-                <div className={`h-[90vh] w-[90vw] text-white flex justify-center items-center overflow-hidden`}>
-                    <div className={`${b.image_container}`}>
-                        <img src="/Images/myimage.jpg" className='rounded-2xl' alt="no image" width="100%"/>
+            <div className='relative -z-10 bg-gradient-to-br from-[black] to-[#1f0812] flex flex-col items-center justify-center'>
+                <div className={`${b.image_codntainer} h-[100vh] w-[100vw] p-[4rem] text-white flex flex-col overflow-hidden justify-center items-center`}>
+                    <div className={`w-[80%] overflow-hidden flex items-center justify-center bg-[white] rounded-full filter grayscale-[0.75]`}>
+                        <img src="/Images/myimage.jpg" alt="no image" width="50%" className=''/>
                     </div>
-                    <div className='relative flex flex-col p-2'>
+                    <div className='w-[90%] h-[50%] flex flex-col p-2 relative'>
                         <p className='text-xl text-pink-600 font-bold'>Hello,<br />I'm Darshan Dinesh MP</p>
-                        <p className="text-3xl font-extrabold">Devoted to the art of programming</p>
+                        <p className="text-3xl font-extrabold">Devoted to the art of programming.</p>
                     </div>
                 </div>
             </div>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -9,13 +9,14 @@ function Nav() {
     }
     return (
         <>
-            <nav className='relative top-0'>
-                <div className='bg-black text-white flex items-center justify-end py-4 px-8'>
-                    <div className='text-lg'><FontAwesomeIcon className='cursor-pointer' onClick={changeBarsClicked} icon={barsClicked ? faClose : faBars} /></div>
+            <nav className='fixed w-full top-0'>
+                <div className='bg-[black] text-white flex items-center justify-end py-4 px-8'>
+                    <div className='text-lg'><FontAwesomeIcon className='cursor-pointer' onClick={changeBarsClicked} icon={faBars} /></div>
                 </div>
-                <div className={`bg-black w-full text-white absolute duration-300 ${barsClicked ? 'scale-y-1 top-[3.5rem]' : 'scale-y-0 -top-[13rem]'}`}>
+                <div className={`bg-black w-full text-white absolute duration-300 ${barsClicked ? 'scale-y-1 top-[0rem]' : 'scale-y-0 -top-[13rem]'}`}>
                     <ul className='w-full text-lg'>
-                        <li className='py-4 px-8 border-b-[1px] border-t-[1px] border-gray-700'><a href="#home">Home</a></li>
+                        <li className='py-4 px-8 border-b-[1px] border-t-[1px] border-gray-700 text-right'><FontAwesomeIcon className='cursor-pointer' onClick={changeBarsClicked} icon={faClose} /></li>
+                        <li className='py-4 px-8 border-b-[1px] border-gray-700'><a href="#home">Home</a></li>
                         <li className='py-4 px-8 border-b-[1px] border-gray-700'><a href="#contact">Contact</a></li>
                         <li className='py-4 px-8 border-b-[1px] border-gray-700'><a href="www.google.com">Projects</a></li>
                     </ul>
