cca3d5c27d8339b948f5ba2fdd15776827aafa74
devil-striker <darshaninsta360@gmail.com>
2023-10-30 20:02:04 +0530

    s

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
     return (
         <>
             <div className='relative -z-10 bg-gradient-to-br from-[black] to-[#1f0812] flex flex-col items-center justify-center'>
-                <div className={`${b.image_codntainer} h-[100vh] w-[100vw] p-[3rem] text-white flex flex-col overflow-hidden justify-center items-center`}>
+                <div className={`${b.image_codntainer} h-[100vh] w-[100vw] p-[3rem] text-white flex flex-col overflow-hidden justify-center items-center gap-4`}>
                     <div className={`w-[70%] overflow-hidden flex items-center justify-center bg-[white] rounded-full filter grayscale-[0.75]`}>
                         <img src="/Images/myimage.jpg" alt="no image" width="50%" className=''/>
                     </div>
