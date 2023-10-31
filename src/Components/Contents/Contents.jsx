199cf00a06c716873ece33c5bd93ff5460dba43d
devil-striker <darshaninsta360@gmail.com>
2023-10-31 23:23:50 +0530

    s

--- src/Components/Contents/Contents.jsx
+++ src/Components/Contents/Contents.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Contents() {
     return (
-        <div>
+        <div className="bg-[#1f0812] p-4 flex flex-col justify-center items-center gap-4">
             {/* Experience */}
-            <div className="p-8 bg-gray-200">
+            <div className="p-8 bg-gray-200 border-2 rounded-xl">
                 <p className="text-3xl font-bold">Experience</p>
                 <div className='p-4 flex flex-col gap-4 text-gray-800'>
-                    <p className="text-2xl font-bold leading-[1.5rem] ">Coordinated Coding Competetion</p>
+                    <p className="text-2xl font-bold leading-[1.75rem]">Coordinated Coding Competetion</p>
                     <div>
                         <p className="text-lg">
                             <span className='font-bold'>Organization:</span><br />St. Aloysius College (Autonomous)
@@ -36,39 +36,31 @@ function Contents() {
             </div>
 
             {/* Projects and Work Samples Section */}
-            <section id="projects" className="py-12 bg-white">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-semibold text-gray-800 mb-4">Projects and Work Samples</h2>
-                    {/* Include your project samples and descriptions here */}
-                </div>
-            </section>
+            <div className="p-8 bg-gray-200 border-2 rounded-xl">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">Projects and Work Samples</h2>
+                {/* Include your project samples and descriptions here */}
+            </div>
 
             {/* Skills and Expertise Section */}
-            <section id="skills" className="py-12 bg-gray-100">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-semibold text-gray-800 mb-4">Skills and Expertise</h2>
-                    {/* List your skills and expertise here */}
-                </div>
-            </section>
+            <div className="p-8 bg-gray-200 border-2 rounded-xl">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">Skills and Expertise</h2>
+                {/* List your skills and expertise here */}
+            </div>
 
             {/* Contact Information Section */}
-            <section id="contact" className="py-12 bg-gray-100">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-semibold text-gray-800 mb-4">Contact Information</h2>
-                    {/* Include your contact information here */}
-                </div>
-            </section>
+            <div className="p-8 bg-gray-200 border-2 rounded-xl">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">Contact Information</h2>
+                {/* Include your contact information here */}
+            </div>
 
             {/* About Me/Personal Statement Section */}
-            <section id="about" className="py-12 bg-white">
-                <div className="container mx-auto">
-                    <h2 className="text-3xl font-semibold text-gray-800 mb-4">About Me/Personal Statement</h2>
-                    {/* Write your personal statement here */}
-                </div>
-            </section>
+            <div className="p-8 bg-gray-200 border-2 rounded-xl">
+                <h2 className="text-3xl font-semibold text-gray-800 mb-4">About Me/Personal Statement</h2>
+                {/* Write your personal statement here */}
+            </div>
 
             {/* Education and Certifications Section */}
-            <div className="container mx-auto">
+            <div className="p-8 bg-gray-200 border-2 rounded-xl">
                 <h2 className="text-3xl font-semibold text-gray-800">Education and Certifications</h2>
                 Secured first place in National level coding competition (09/2022)
                 Secured first place in Coding Event in the National level IT Fest "Joshiana 11.0" organized by St Joseph
