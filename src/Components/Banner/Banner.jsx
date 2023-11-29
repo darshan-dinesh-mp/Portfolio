3d495fea8b04ea1a2700bc026328cc3ad806021c
Darshan Dinesh <darshandineshmp@gmail.com>
2023-11-29 18:01:31 +0530

    hello

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner() {
     return (
         <>
             <div className='relative h-[100vh] w-[100vw] -z-10 bg-gradient-to-b from-[black] to-[#1f0812] flex flex-col items-center justify-center'>
-                <div className={`${b.image_codntainer} w-[70%] p-[2rem] text-white flex flex-col gap-4 justify-center items-center border-[0.5px] rounded-2xl`}>
+                <div className={`${b.image_codntainer} w-[70%] p-[2rem] text-white flex flex-col gap-4 justify-center items-center rounded-2xl`}>
                     <div className={`w-[100%] overflow-hidden flex items-center justify-center bg-[white] rounded-full filter grayscale-[0.75]`}>
                         <img src="/Images/myimage.jpg" alt="no image" width="50%" className=''/>
                     </div>
